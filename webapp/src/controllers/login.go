6f16df554c4ac7bcbb6f6b52bf159085c2ca7651
João Pedro <joaopedrosouzapeixotosaraiva@outlook.com>
2024-03-28 15:59:05 -0300

    Atualização

--- webapp/src/controllers/login.go
+++ webapp/src/controllers/login.go
@@ -3,17 +3,17 @@ package controllers
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"webapp/src/respostas"
 )
 
-func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+func FazerLogin(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
-	usuarios, erro := json.Marshal(map[string]string{
-		"nome":  r.FormValue("nome"),
+	usuario, erro := json.Marshal(map[string]string{
 		"email": r.FormValue("email"),
-		"nick":  r.FormValue("nick"),
 		"senha": r.FormValue("senha"),
 	})
 	if erro != nil {
@@ -21,17 +21,15 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	response, erro := http.Post("http://10.1.1.33:5000/usuarios", "application/json", bytes.NewBuffer(usuarios))
+	response, erro := http.Post("http://10.1.1.33:5000/login", "application/json", bytes.NewBuffer(usuario))
 	if erro != nil {
 		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
 		return
 	}
 	defer response.Body.Close()
 
-	if response.StatusCode >= 400 {
-		respostas.TratarErro(w, response)
-		return
-	}
+	token, _ := io.ReadAll(response.Body)
 
-	respostas.JSON(w, response.StatusCode, nil)
+	fmt.Println(response.StatusCode)
+	fmt.Println(string(token))
 }
